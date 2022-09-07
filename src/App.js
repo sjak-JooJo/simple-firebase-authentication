79cd193f7f442746f5a87831c3f97ef86ca9136f
JooJo <sjak0101@gmail.com>
2022-09-07 15:18:29 +0100

    added github authentication

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 
-import { getSpaceUntilMaxLength } from '@testing-library/user-event/dist/utils';
-import { FirebaseError } from 'firebase/app';
-import './App.css';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase.init';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import './App.css';
+import { useState } from "react";
 
 
 const auth = getAuth(app);
 
 function App() {
-  const provider = new GoogleAuthProvider();
-  const handleGooglesignIn = () =>{
-    signInWithPopup(auth, provider)
+  const [user, setUser] = useState({})
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleGoogleSignIn = () =>{
+
+    signInWithPopup(auth, googleProvider)
     .then(result =>{
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
     .catch(error =>{
-      console.log('error',error);
+      console.error('Error', error);
     })
   }
+
+
+
+  const handleGithubSignIn = () =>{
+    signInWithPopup(auth, githubProvider)
+    .then(result =>{
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .error(error =>{
+      console.error(error);
+    })
+  }
+
+  const handleSignOut = () =>{
+    signOut(auth)
+    .then( () =>{
+      setUser({});
+    })
+    .catch(error =>{
+      setUser({});
+    })
+  }
+
   return (
     <div className="App">
-       <button onClick={handleGooglesignIn}>Google Sign in</button>
+      {/* {condition ? true : false} */}
+      {
+        user.email ? <button onClick={handleSignOut}>Sign Out</button>
+        :
+        <>
+          <button onClick={handleGoogleSignIn} >Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+        
+      }
+      <h2>Name : {user.displayName}</h2>
+      <p>I know your email address: {user.email}</p>
+      <img src={user.photoURL} alt="profile pic" />
     </div>
   );
 }
--- src/_steps.js
+++ src/_steps.js
@@ -4,10 +4,11 @@
 * 2. install firebase npm install 
 * 3. Register 
 * 4. copy firebase init with config from firebase project settings into a file firebase.init.js
-* 5. export default app
-* 6. import app firebase init.js into your app.js
-* 7. import getAuth from firebase/auth and create auth = getAuth(app)
+* 5. export default app from firebase.init.js
+* 6. import getAuth from firebase/auth and create const auth = getAuth(app) in app.js
+* 7. import app firebase init.js into your app.js
 * 8. turn on google authetication (firebase > autehntication > google sign in)
 * 9. create google provider
-* 10. 
+* 10. use signInWithPopup and pass auth and provider
+* 11. handle .then (if successful) and catch error (if error)
 */
